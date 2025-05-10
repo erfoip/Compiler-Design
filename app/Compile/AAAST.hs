0f810260fdf0d4d0083238c9cf423cf78cb531ee
Peter Achinger <peter.achinger@yahoo.com>
2025-05-10 16:20:14 +0200

    Implement instruction selection

--- app/Compile/AAAST.hs
+++ app/Compile/AAAST.hs
@@ -10,17 +10,20 @@ module Compile.AAAST
 import Compile.AST (Op)
 import Data.List (intercalate)
 
+type Register = Integer
+
 data AAAST =
     Block [Inst]
 
 data Inst
-    = Init String Operand
-    | Asgn String Operand Op Operand
+    = Init Register Operand -- d <- s
+    | Asgn Register Op Operand -- d <- d Op s
+    | UnOpAsgn Register Op -- d <- Op d
+    | Ret Operand
 
 data Operand
-    = Reg Integer
+    = Reg Register
     | Con Integer
-    | Temp String
 
 
 instance Show AAAST where
@@ -28,10 +31,11 @@ instance Show AAAST where
         "Block: {\n" ++ intercalate "\n" (map show insts) ++ "\n}"
 
 instance Show Inst where
-    show (Init dest op) = "Init: " ++ dest ++ " <- " ++ show op
-    show (Asgn dest op1 op op2) = "Assign: " ++ dest ++ " <- " ++ show op1 ++ " " ++ show op ++ " " ++ show op2
+    show (Init dest op) = "Init: " ++ "R" ++ show dest ++ " <- " ++ show op
+    show (Asgn dest op op1) = "Assign: " ++ "R" ++ show dest ++ " <- " ++ "R" ++ show dest ++ " " ++ show op ++ " " ++ show op1
+    show (UnOpAsgn dest op) = "UnOpAssign" ++ "R" ++ show dest ++ " <- " ++ show op ++ " " ++ show op
+    show (Ret op1) = "Return " ++ show op1
 
 instance Show Operand where
     show (Reg i) = 'R' : show i
     show (Con c) = show c
-    show (Temp t) = t
--- app/Compile/AAsm.hs
+++ app/Compile/AAsm.hs
@@ -1,34 +1,30 @@
 module Compile.AAsm
-  ( codeGen
-  ) where
+ ( codeGen
+ ) where
 
-import           Compile.AST (AST(..), Expr(..), Stmt(..), showAsgnOp)
-
-import           Control.Monad.State
+import Compile.AST (AST(..), Expr(..), Stmt(..), Op, showAsgnOp)
+import Compile.AAAST (AAAST(..), Inst(..), Operand(..))
+import Control.Monad.State
 import qualified Data.Map as Map
 
-type Register = Integer
-
 type VarName = String
-
-type AAsmAlloc = Map.Map VarName Register
-
+type Register = Integer
+type RegisterMap = Map.Map VarName Register
 type CodeGen a = State CodeGenState a
 
 data CodeGenState = CodeGenState
-  { regMap :: AAsmAlloc
-  , nextReg :: Register
-  , code :: [String]
-  }
+ { regMap :: RegisterMap
+ , nextReg :: Register
+ , code :: [Inst]
+ }
 
-codeGen :: AST -> [String]
-codeGen (Block stmts _) = code $ execState (genBlock stmts) initialState
+-- | Main entry point for code generation
+codeGen :: AST -> AAAST
+codeGen (Compile.AST.Block stmts _) = Compile.AAAST.Block $ code $ execState (genBlock stmts) initialState
   where
     initialState = CodeGenState Map.empty 0 []
 
-regName :: Register -> String
-regName n = "%" ++ show n
-
+-- | Generate a fresh register
 freshReg :: CodeGen Register
 freshReg = do
   curr <- get
@@ -36,52 +32,93 @@ freshReg = do
   put curr {nextReg = r + 1}
   return r
 
+-- | Assign a register to a variable
 assignVar :: VarName -> Register -> CodeGen ()
-assignVar name r = do
-  modify $ \s -> s {regMap = Map.insert name r (regMap s)}
+assignVar name op = do
+  modify $ \s -> s {regMap = Map.insert name op (regMap s)}
 
+-- | Look up a variable's operand
 lookupVar :: VarName -> CodeGen Register
 lookupVar name = do
   m <- gets regMap
-  case Map.lookup name m of
-    Just r -> return r
-    Nothing -> error "unreachable, fix your semantic analysis I guess"
+  maybe freshReg return (Map.lookup name m)
 
-emit :: String -> CodeGen ()
-emit instr = modify $ \s -> s {code = code s ++ [instr]}
+-- | Add an instruction to the code
+emit :: Inst -> CodeGen ()
+emit inst = modify $ \s -> s {code = code s ++ [inst]}
 
+-- | Process a block of statements
 genBlock :: [Stmt] -> CodeGen ()
 genBlock = mapM_ genStmt
 
+-- | Process a statement
 genStmt :: Stmt -> CodeGen ()
 genStmt (Decl name _) = do
   r <- freshReg
+  emit $ Compile.AAAST.Init r $ Con 0
   assignVar name r
-genStmt (Init name e _) = do
-  r <- genExpr e
-  assignVar name r
-genStmt (Asgn name op e _) = do
-  rhs <- genExpr e
-  lhs <- lookupVar name
-  emit $ regName lhs ++ showAsgnOp op ++ regName rhs
-genStmt (Ret e _) = do
-  r <- genExpr e
-  emit $ "ret " ++ regName r
 
-genExpr :: Expr -> CodeGen Register
-genExpr (IntExpr n _) = do
+genStmt (Compile.AST.Init name e _) = do
+  op <- genExpr e
   r <- freshReg
-  emit $ regName r ++ " = " ++ show n
-  return r
-genExpr (Ident name _) = lookupVar name
+  emit $ Compile.AAAST.Init r op
+  assignVar name r
+
+genStmt (Compile.AST.Asgn name (Just op) e _) = do
+  rhs <- genExpr e
+  r <- lookupVar name
+
+  -- Maximum munch for assignment
+  case (r, rhs) of
+    -- When both sides are operands, emit a direct assignment
+    (_, _) ->
+      -- For simplicity, we'll use a base case that handles everything
+      -- In a full maximum munch implementation, you would add specific pattern matches here
+      emit $ Compile.AAAST.Asgn r op rhs
+
+genStmt (Compile.AST.Asgn name Nothing e _) = do
+  rhs <- genExpr e
+  r <- lookupVar name
+  emit $ Compile.AAAST.Init r rhs
+
+
+genStmt (Compile.AST.Ret e _) = do
+  -- Handle return statement (could be adapted based on your target language)
+  -- For now, we'll just evaluate the expression
+  rhs <- genExpr e
+  emit $ Compile.AAAST.Ret rhs
+
+-- | Process an expression using maximum munch
+genExpr :: Expr -> CodeGen Operand
+genExpr (IntExpr n _) =
+  -- Constants are represented directly
+  return $ Con n
+
+genExpr (Ident name _) = do
+  r <- lookupVar name
+  return $ Reg r
+
 genExpr (UnExpr op e) = do
-  r1 <- genExpr e
-  r <- freshReg
-  emit $ regName r ++ " = " ++ show op ++ " " ++ regName r1
-  return r
+  opnd <- genExpr e
+  -- For unary operations, we can use a dummy second operand (or adapt the AAAST to support unary ops)
+  -- Using Con 0 as a placeholder, but this should be adapted to your needs
+  case opnd of
+    Reg r -> 
+      emit $ UnOpAsgn r op    
+    Con i -> do
+      r <- freshReg
+      emit $ Compile.AAAST.Init r (Con i)
+      emit $ UnOpAsgn r op
+  return opnd
+
 genExpr (BinExpr op e1 e2) = do
-  r1 <- genExpr e1
-  r2 <- genExpr e2
+  opnd1 <- genExpr e1
+  opnd2 <- genExpr e2
   r <- freshReg
-  emit $ regName r ++ " = " ++ regName r1 ++ " " ++ show op ++ " " ++ regName r2
-  return r
+
+  -- Maximum munch for binary operations
+  -- Here we can add specific patterns for optimal instruction selection
+  -- For now, we'll use a general case
+  emit $ Compile.AAAST.Init r opnd1
+  emit $ Compile.AAAST.Asgn r op opnd2
+  return $ Reg r
